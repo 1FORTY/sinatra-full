a581ecfd29f0a2ea38eb52be81902870575f3b4e
Ivan Sklyarenko <wanya.workmail@gmail.com>
2020-10-01 16:07:14 +0700

    Correct link on app.rb

--- app.rb
+++ app.rb
@@ -8,5 +8,5 @@ require 'sinatra/activerecord'
 # set :database, { adapter: 'sqlite3', database: 'your_db.db' }
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\"  target=\"_blank\">Original</a> pattern has been modified for <a href=\"https://github.com/1FORTY\" target=\"_blank\">Ruby School</a>"
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\"  target=\"_blank\">Original</a> pattern has been modified for <a href=\"https://github.com/1FORTY\" target=\"_blank\">author's GitHub</a>"
 end
